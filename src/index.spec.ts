688369b82b274174437f5d1f332d53b91eaa69e6
yijie4188 <yijie4188@gmail.com>
2023-12-11 23:59:01 +0800

    refactor: use `describe` and `test` to split unit test cases

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,43 +1,54 @@
-import { expectTypeOf } from 'vitest'
+import { test, expectTypeOf, describe } from 'vitest'
 
 import { t } from './index'
 
-expectTypeOf(t(String)).toEqualTypeOf<t.Schema<StringConstructor, string>>()
-expectTypeOf(t()).toEqualTypeOf<t.Schema<any, any>>()
+test('primitive', () => {
+  expectTypeOf(t(String)).toEqualTypeOf<t.Schema<StringConstructor, string>>()
+  expectTypeOf(t()).toEqualTypeOf<t.Schema<any, any>>()
+})
 
-const NumberArrSkm = t(Array, Number)
-const NumberVectorSkm = t(Array, Array, Number)
-
-expectTypeOf(t(Array)).toEqualTypeOf<t.Schema<
-  t.Schema<any, any>[], any[]
->>()
-expectTypeOf(NumberArrSkm)
-  .toEqualTypeOf<
-    t.Schema<
+describe('array and tuple', () => {
+  test('array', () => {
+    expectTypeOf(t(Array)).toEqualTypeOf<t.Schema<
+      t.Schema<any, any>[], any[]
+    >>()
+    expectTypeOf(t(Array, Number))
+      .toEqualTypeOf<
+        t.Schema<
+          t.Schema<NumberConstructor, number>[],
+          number[]
+        >
+      >()
+    expectTypeOf(t(Array, Array, Number))
+      .toEqualTypeOf<
+        t.Schema<
+          t.Schema<t.Schema<NumberConstructor, number>[], number[]>[],
+          number[][]
+        >
+      >()
+  })
+  test('tuple', () => {
+    expectTypeOf(t([])).toEqualTypeOf<t.Schema<[], []>>()
+    expectTypeOf(t([], Number)).toEqualTypeOf<t.Schema<
       t.Schema<NumberConstructor, number>[],
       number[]
-    >
-  >()
-expectTypeOf(NumberVectorSkm)
-  .toEqualTypeOf<
-    t.Schema<
+    >>()
+    expectTypeOf(t([], [], Number)).toEqualTypeOf<t.Schema<
       t.Schema<t.Schema<NumberConstructor, number>[], number[]>[],
       number[][]
-    >
-  >()
+    >>()
+  })
+})
 
-expectTypeOf(t([])).toEqualTypeOf<t.Schema<[], []>>()
-expectTypeOf(t([], Number)).toEqualTypeOf<t.Schema<
-  t.Schema<NumberConstructor, number>[],
-  number[]
->>()
-
-const NumberMapToStringSkm = t(Object, Number, String)
-expectTypeOf(NumberMapToStringSkm)
-  .toEqualTypeOf<
-    t.Schema<{
-      [x: number]: t.Schema<StringConstructor, string>;
-    }, {
-      [x: number]: string;
-    }>
-  >()
+describe('object', () => {
+  test('object', () => {
+    expectTypeOf(t(Object, Number, String))
+      .toEqualTypeOf<
+        t.Schema<{
+          [x: number]: t.Schema<StringConstructor, string>;
+        }, {
+          [x: number]: string;
+        }>
+      >()
+  })
+})
