024929aef62583d25abdd55a6e1c3a161e439149
yijie4188 <yijie4188@gmail.com>
2023-12-11 22:52:19 +0800

    refactor: abstract ConstructorEntries type

--- src/index.ts
+++ src/index.ts
@@ -13,33 +13,42 @@ namespace Stack {
     [...T, V]
 }
 
+type ConstructorEntries<
+  T,
+  Rest extends any[]
+> = IsEqual<T, ArrayConstructor> extends true ? (
+  t.Schema<
+    Typp<Rest>[],
+    t.Infer<Typp<Rest>>[]
+  >
+) : IsEqual<T, ObjectConstructor> extends true ? (
+  Stack.Shift<Rest> extends [
+    infer L extends StringConstructor | NumberConstructor | SymbolConstructor,
+    infer Rest extends any[]
+  ] ? (
+    // @ts-ignore
+    t.Infer<Typp<[L]>> extends (
+      infer Keys extends string | number | symbol
+    ) ? t.Schema<{
+        // TODO union
+        [k in Keys]: Typp<Rest>
+      }, {
+        // TODO union
+        [k in Keys]: t.Infer<Typp<Rest>>
+      }>
+      : never
+  ) : never
+) : never
+
 type Typp<T extends any[]> = IsEqual<T, []> extends true
   ? any
   : (
     Stack.Shift<T> extends [infer L, infer Rest extends any[]]
-      ? L extends ArrayConstructor ? (
-        t.Schema<
-          Typp<Rest>[],
-          t.Infer<Typp<Rest>>[]
-        >
-      )
-      : L extends ObjectConstructor ? (
-        Stack.Shift<Rest> extends [
-          infer L extends StringConstructor | NumberConstructor | SymbolConstructor,
-          infer Rest extends any[]
-        ] ? (
-          t.Infer<Typp<[L]>> extends infer Keys extends string | number | symbol
-            ? t.Schema<{
-              // TODO union
-              [k in Keys]: Typp<Rest>
-            }, {
-              // TODO union
-              [k in Keys]: t.Infer<Typp<Rest>>
-            }>
-            : never
-        ) : never
-      )
-      : t.Schema<L, ConstructorMapping<L>>
+      ? ConstructorEntries<L, Rest> extends (infer R)
+        ? [R] extends [never]
+          ? t.Schema<L, ConstructorMapping<L>>
+          : R
+        : never
       : never
   )
 
@@ -47,9 +56,6 @@ type Typp<T extends any[]> = IsEqual<T, []> extends true
 // &./index.spec.ts:10:7?
 // &./index.spec.ts:11:7?
 // &./index.spec.ts:12:7?
-
-type T0 = Typp<[ObjectConstructor, NumberConstructor, StringConstructor]>['shape']
-//   ^?
 
 export function t<T extends any[]>(...t: T): Typp<T> {
   return {} as Typp<T>
