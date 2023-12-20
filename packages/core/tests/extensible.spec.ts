9ef36438523fc9dac098de3481809a6fdf2a67de
yijie4188 <yijie4188@gmail.com>
2023-12-20 13:54:28 +0800

    feat(core): supplement more unit test for `defineStatic` about throw Error when refine

--- packages/core/tests/extensible.spec.ts
+++ packages/core/tests/extensible.spec.ts
@@ -19,3 +19,14 @@ test('base', () => {
   dispose = t.defineStatic('____foo', 'foo')
   dispose()
 })
+test('throw Error when static field is existed', () => {
+  t.defineStatic('____bar', 1)
+  expect(() => {
+    t.defineStatic('____bar', 2)
+  }).toThrow()
+})
+test('when override is true, will override the existed static field and not throw Error', () => {
+  t.defineStatic('____bar', 1)
+  t.defineStatic('____bar', 2, { override: true })
+  expect(t.____bar).toBe(2)
+})
