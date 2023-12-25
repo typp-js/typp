5bed5e445b2d5e74ad8436c62f9125c78c25f67f
yijie4188 <yijie4188@gmail.com>
2023-12-25 17:05:01 +0800

    refactor(core): simplify `IsNotEqual`

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -10,7 +10,9 @@ export type IsEqual<A, B> =
   (<T>() => T extends A ? 1 : 2) extends
   (<T>() => T extends B ? 1 : 2) ? true : false
 
-export type IsNotEqual<A, B> = IsEqual<A, B> extends true ? false : true
+export type IsNotEqual<A, B> =
+  (<T>() => T extends A ? 1 : 2) extends
+  (<T>() => T extends B ? 1 : 2) ? false : true
 
 export type Includes<
   T extends readonly any[],
