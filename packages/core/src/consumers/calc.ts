47f6a542348ff922855455f6e095d569d49b3645
yijie4188 <yijie4188@gmail.com>
2024-01-07 20:32:18 +0800

    style(core): code style

--- packages/core/src/consumers/calc.ts
+++ packages/core/src/consumers/calc.ts
@@ -10,21 +10,23 @@ declare module '../base' {
     // TODO exclude
     // TODO extract
     export interface ShapeEntries<T, Rest extends any[]> {
-      300000: [(
+      300000: [
         [T] extends [t.SpecialShape<t.SpecialShapeTypes, any>] ? (
           IsEqual<T['type'], t.SpecialShapeTypeMapping['union']>
-          ) : false
-        ), [T] extends [t.SpecialShape<t.SpecialShapeTypes, any>] ? (
-        // TODO no `t.InferT`?
-        t.Union<t.InferT<T['schemas']>>
-        ) : never]
-      300001: [(
+        ) : false,
+        [T] extends [t.SpecialShape<t.SpecialShapeTypes, any>] ? (
+          // TODO no `t.InferT`?
+          t.Union<t.InferT<T['schemas']>>
+        ) : never
+      ]
+      300001: [
         [T] extends [t.SpecialShape<t.SpecialShapeTypes, any>] ? (
           IsEqual<T['type'], t.SpecialShapeTypeMapping['intersection']>
-          ) : false
-        ), [T] extends [t.SpecialShape<t.SpecialShapeTypes, any>] ? (
-        Intersect<T['schemas']>
-        ) : never]
+        ) : false,
+        [T] extends [t.SpecialShape<t.SpecialShapeTypes, any>] ? (
+          Intersect<T['schemas']>
+        ) : never
+      ]
     }
     export interface DynamicSpecialShapeTypeMapping {
       readonly union: typeof unionSymbol
