5cd0bd185768ee201c99b241fc9ae9f9e23bb9e0
yijie4188 <yijie4188@gmail.com>
2024-01-26 23:11:13 +0800

    feat(validator): init validator module

---
+++ packages/validator/src/index.ts
@@ -0,0 +1,118 @@
+import type { IsEqual, Narrow, t as tn } from '@typp/core'
+
+declare module '@typp/core' {
+  namespace t {
+    export const coerce: typeof t
+    interface ParseOptions {
+      strict?: boolean
+    }
+    export type ParseSuccess<T> = {
+      success: true
+      data: T
+    }
+    export type ParseError = {
+      success: false
+      error: ValidateError
+    }
+    export type ParseResult<T> = ParseSuccess<T> | ParseError
+    interface SchemaFieldsAll<Shape, T> {
+      /**
+       * 根据数据定义格式校验数据，当数据满足格式时返回数据本身，引用不变化。
+       * 但是如果数据不满足格式，则抛出校验异常，外部需要对校验异常和可能存在的其他异常进行处理。
+       */
+      parse: (
+        & ((data: T) => T)
+        & {
+          narrow<TT extends T>(data: Narrow<TT>): TT
+        }
+      )
+      /**
+       * 与 parse 函数类似，但是在出现异常时会将校验异常捕获并包装后返回。
+       */
+      tryParse: (
+        & (<Rest>(data: T | Rest) => (
+          true extends (
+            | IsEqual<Rest, any>
+            | IsEqual<Rest, unknown>
+          ) ? ParseResult<T>
+            : [Rest] extends [T] ? ParseSuccess<T> : ParseError
+        ))
+        & {
+          narrow<TT extends T>(data: Narrow<TT>): ParseSuccess<TT>
+        }
+      )
+      // for zod
+      safeParse: this['tryParse']
+    }
+  }
+}
+
+export class ValidateError extends Error {
+  __TYPP_SYMBOL__ = '__ValidateError__'
+  constructor(message: string) {
+    super(message)
+    this.name = 'ValidateError'
+  }
+}
+
+function parse(this: tn.Schema<any, any>, data: any) {
+  if (this.shape === Number) {
+    if (typeof data !== 'number') {
+      throw new ValidateError(`Expected number, but got \`${JSON.stringify(data)}\``)
+    }
+  }
+  return data
+}
+parse.narrow = parse
+
+function catchAndWrap(func: Function): tn.ParseResult<any> {
+  try {
+    return { success: true, data: func() }
+  } catch (error) {
+    const e = error as any
+    if (
+      e instanceof ValidateError
+      /* istanbul ignore next */
+      || e?.__TYPP_SYMBOL__ === '__ValidateError__'
+    ) {
+      return { success: false, error: e }
+    }
+    throw error
+  }
+}
+function catchAndWrapProxy<T extends Function>(func: T, proxyHandler: Omit<ProxyHandler<any>, 'apply'> = {}): T {
+  return new Proxy(func, {
+    ...proxyHandler,
+    apply(target, thisArg, args) {
+      return catchAndWrap(() => Reflect.apply(target, thisArg, args))
+    }
+  })
+}
+
+export default function validator(t: typeof tn) {
+  t.useFields({
+    get parse() {
+      // eslint-disable-next-line @typescript-eslint/no-this-alias
+      const skm = this
+      return new Proxy(parse, {
+        get(target, key) {
+          return (
+            Reflect.get(target, key) as Function
+          ).bind(skm)
+        }
+      })
+    },
+    get tryParse() {
+      // eslint-disable-next-line @typescript-eslint/no-this-alias
+      const skm = this
+      return catchAndWrapProxy(parse, {
+        get(target, key) {
+          return catchAndWrapProxy((
+            Reflect.get(target, key) as Function
+          ).bind(skm))
+        }
+      })
+    }
+  })
+}
+export { validator }
---
+++ packages/validator/tests/index.spec.ts
@@ -0,0 +1,56 @@
+import { t } from '@typp/core'
+import { beforeAll, describe, expect, expectTypeOf, test } from 'vitest'
+
+import validator, { ValidateError } from '../src'
+
+describe('parse', () => {
+  beforeAll(() => t.use(validator))
+  test('base', () => {
+    const NumberSchema = t.number()
+    const a = NumberSchema.parse(1)
+    expect(a).toBe(1)
+    expectTypeOf(a).toEqualTypeOf<number>()
+    const b = NumberSchema.parse.narrow(1)
+    expect(b).toBe(1)
+    expectTypeOf(b).toEqualTypeOf<1>()
+  })
+  test('unexpected', () => {
+    const NumberSchema = t.number()
+    // @ts-expect-error
+    expect(() => NumberSchema.parse('1')).toThrow(ValidateError)
+    // @ts-expect-error
+    expect(() => NumberSchema.parse.narrow('1')).toThrow(ValidateError)
+  })
+  test('tryParse', () => {
+    const NumberSchema = t.number()
+    const result0 = NumberSchema.tryParse(1)
+    expect(result0.success).toBe(true)
+    expect(result0.data).toBe(1)
+    expectTypeOf(result0).toEqualTypeOf<t.ParseSuccess<number>>()
+
+    const result1 = NumberSchema.tryParse('1')
+    expect(result1.success).toBe(false)
+    expect(result1.error).toBeInstanceOf(ValidateError)
+    expectTypeOf(result1).toEqualTypeOf<t.ParseError>()
+
+    const result2 = NumberSchema.tryParse({} as any)
+    expectTypeOf(result2).toEqualTypeOf<t.ParseResult<number>>()
+    if (result2.success) {
+      expectTypeOf(result2).toEqualTypeOf<t.ParseSuccess<number>>()
+    } else {
+      expectTypeOf(result2).toEqualTypeOf<t.ParseError>()
+      expect(result2.success).toBe(false)
+      expect(result2.error).toBeInstanceOf(ValidateError)
+    }
+
+    const result3 = NumberSchema.tryParse({} as unknown)
+    expectTypeOf(result3).toEqualTypeOf<t.ParseResult<number>>()
+    if (result3.success) {
+      expectTypeOf(result3).toEqualTypeOf<t.ParseSuccess<number>>()
+    } else {
+      expectTypeOf(result3).toEqualTypeOf<t.ParseError>()
+      expect(result3.error).toBeInstanceOf(ValidateError)
+    }
+    expect(result2.success).toBe(false)
+  })
+})
