472c8d564a4586950e88e03dd2d0a3cc7d458b09
yijie4188 <yijie4188@gmail.com>
2024-02-01 22:41:17 +0800

    feat(validator): string transform to number

--- packages/validator/tests/index.spec.ts
+++ packages/validator/tests/index.spec.ts
@@ -91,11 +91,70 @@ describe('parse', () => {
           skm.validate('abc')
         }).toThrow(new ValidateError('unexpected', skm, '1'))
       })
-      test('transform', () => {
+      test('transform - string', () => {
         const skm = t.number()
-        const r = skm.parse('1')
-        expect(r).toBe(1)
-        expectTypeOf(r).toEqualTypeOf<number>()
+        const r0 = skm.parse('1')
+        expect(r0).toBe(1)
+        expectTypeOf(r0).toEqualTypeOf<number>()
+
+        // special number
+        const r1 = skm.parse('NaN')
+        expect(r1).toBeNaN()
+        expectTypeOf(r1).toEqualTypeOf<number>()
+        const r2 = skm.parse('Infinity')
+        expect(r2).toBe(Infinity)
+        expectTypeOf(r2).toEqualTypeOf<number>()
+        const r3 = skm.parse('-Infinity')
+        expect(r3).toBe(-Infinity)
+        expectTypeOf(r3).toEqualTypeOf<number>()
+
+        // special radix
+        const r4 = skm.parse('0b10')
+        const r5 = skm.parse('0B10')
+        expect(r4).toBe(2)
+        expect(r4).toBe(r5)
+        expectTypeOf(r4).toEqualTypeOf<number>()
+        expectTypeOf(r4).toEqualTypeOf<typeof r5>()
+        const r6 = skm.parse('0o10')
+        const r7 = skm.parse('0O10')
+        expect(r6).toBe(8)
+        expect(r6).toBe(r7)
+        expectTypeOf(r6).toEqualTypeOf<number>()
+        expectTypeOf(r6).toEqualTypeOf<typeof r7>()
+        const r8 = skm.parse('0x1b')
+        const r9 = skm.parse('0X1b')
+        expect(r8).toBe(27)
+        expect(r8).toBe(r9)
+        expectTypeOf(r8).toEqualTypeOf<number>()
+        expectTypeOf(r8).toEqualTypeOf<typeof r9>()
+
+        // float
+        const r10 = skm.parse('1.2')
+        expect(r10).toBe(1.2)
+        expectTypeOf(r10).toEqualTypeOf<number>()
+        const r11 = skm.parse('1.2e3')
+        expect(r11).toBe(1200)
+        expectTypeOf(r11).toEqualTypeOf<number>()
+        const r12 = skm.parse('1.2e-3')
+        expect(r12).toBe(0.0012)
+        expectTypeOf(r12).toEqualTypeOf<number>()
+        const r13 = skm.parse('1.2e+3')
+        expect(r13).toBe(1200)
+        expectTypeOf(r13).toEqualTypeOf<number>()
+
+        // negative
+        const r14 = skm.parse('-1.2')
+        expect(r14).toBe(-1.2)
+        expectTypeOf(r14).toEqualTypeOf<number>()
+
+        // empty
+        const r15 = skm.parse('')
+        expect(r15).toBe(0)
+        expectTypeOf(r15).toEqualTypeOf<number>()
+
+        expect(() => {
+          expectTypeOf(skm.parse('abc')).toEqualTypeOf<number>()
+        }).toThrow(new ValidateError('unexpected', skm, 'abc'))
       })
     })
   })
