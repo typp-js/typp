06527c5a41b758880c20eebd5484fcf3a1dbb7c7
yijie4188 <yijie4188@gmail.com>
2024-02-05 19:50:13 +0800

    feat(validator): resolve `Infinity` and `-Infinity`

--- packages/validator/src/index.ts
+++ packages/validator/src/index.ts
@@ -378,6 +378,13 @@ setResolverByShape(Symbol, {
   validate: input => typeof input === 'symbol',
   transform: input => Symbol(String(input))
 })
+// [§21.4.1.1 Time Values and Time Range]
+// > A Number can exactly represent all integers from -9,007,199,254,740,992 to 9,007,199,254,740,992 (21.1.2.8 and 21.1.2.6).
+// > A time value supports a slightly smaller range of -8,640,000,000,000,000 to 8,640,000,000,000,000 milliseconds.
+// > This yields a supported time value range of exactly -100,000,000 days to 100,000,000 days relative to midnight
+// > at the beginning of 1 January 1970 UTC.
+// 100_000_000 * 24 * 60 * 60 * 1000
+const MAX_TIME = 8640000000000000
 setResolverByShape(Date, {
   preprocess,
   validate: input => input instanceof Date,
@@ -390,19 +397,15 @@ setResolverByShape(Date, {
         }
       // eslint-disable-next-line no-fallthrough
       case 'number':
-        if (Number.isFinite(input)) {
-          // TODO resolve to max or min time, or throw transform error of parse error
+        if (input === Infinity) {
+          return new Date(MAX_TIME)
+        } else if (input === -Infinity) {
+          return new Date(-MAX_TIME)
         }
+
         return new Date(input)
       case 'bigint': {
         const num = Number(input)
-        // [§21.4.1.1 Time Values and Time Range]
-        // > A Number can exactly represent all integers from -9,007,199,254,740,992 to 9,007,199,254,740,992 (21.1.2.8 and 21.1.2.6).
-        // > A time value supports a slightly smaller range of -8,640,000,000,000,000 to 8,640,000,000,000,000 milliseconds.
-        // > This yields a supported time value range of exactly -100,000,000 days to 100,000,000 days relative to midnight
-        // > at the beginning of 1 January 1970 UTC.
-        // 100_000_000 * 24 * 60 * 60 * 1000
-        const MAX_TIME = 8640000000000000
         if (num > Number.MAX_SAFE_INTEGER) {
           return new Date(MAX_TIME)
         } else if (num < Number.MIN_SAFE_INTEGER) {
