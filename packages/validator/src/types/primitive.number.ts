81e4e3b896a53284135d3f45d0108390f3becd23
yijie4188 <yijie4188@gmail.com>
2024-02-26 22:00:45 +0800

    refactor(validator): abstract `SwitchBaseType` type-level util function

--- packages/validator/src/types/primitive.number.ts
+++ packages/validator/src/types/primitive.number.ts
@@ -1,7 +1,78 @@
-import type { IsAnySubType, IsEqual, IsSubType, Switch, t as tn } from '@typp/core'
+import type { IsAnySubType, IsEqual, IsSubType, IsWhat, Switch, t as tn } from '@typp/core'
 
 import { FALSY } from '../base'
 import { preprocess } from '../utils.inner'
+
+type BaseTypeNames =
+  | 'bigint'
+  | 'boolean'
+  | 'number'
+  | 'string'
+  | 'symbol'
+  | 'null'
+  | 'undefined'
+type IsAnySubTypeWithSelfName<Key, SelfName extends BaseTypeNames, Input, InputRest, SubType> = IsWhat<Key, SelfName> extends true
+  ? IsAnySubType<Input, SubType>
+  : IsAnySubType<InputRest, SubType>
+interface IsTargetBaseTypeName<SelfName extends BaseTypeNames, Input, InputRest> {
+  bigint: IsAnySubTypeWithSelfName<
+    'bigint',
+    SelfName, Input, InputRest,
+    bigint | BigInt
+  >
+  boolean: IsAnySubTypeWithSelfName<
+    'boolean',
+    SelfName, Input, InputRest,
+    boolean | Boolean
+  >
+  number: IsAnySubTypeWithSelfName<
+    'number',
+    SelfName, Input, InputRest,
+    number | Number
+  >
+  string: IsAnySubTypeWithSelfName<
+    'string',
+    SelfName, Input, InputRest,
+    string | String
+  >
+  symbol: IsAnySubTypeWithSelfName<
+    'symbol',
+    SelfName, Input, InputRest,
+    symbol | Symbol
+  >
+  null: IsSubType<InputRest, null>
+  undefined: IsSubType<InputRest, undefined>
+}
+type BaseTypesTransformers = {
+  [K in BaseTypeNames]: unknown
+}
+declare const anyReturn: unique symbol
+declare const neverReturn: unique symbol
+type SwitchBaseType<
+  Input,
+  InputRest,
+  SelfName extends BaseTypeNames,
+  Transformers extends BaseTypesTransformers,
+  Entries extends { [k: string]: [boolean, any] } = {
+    [K in BaseTypeNames]: [
+      IsTargetBaseTypeName<SelfName, Input, InputRest>[K],
+      Transformers[K]
+    ]
+  }
+> = Switch<{
+  any: [IsEqual<Input, any>, typeof anyReturn]
+
+  never: [IsEqual<Input, never>, typeof neverReturn]
+  unknown: [IsEqual<Input, unknown>, unknown]
+}> extends infer R
+  ? IsWhat<R, never> extends true
+    ? Switch<Entries>
+    : IsWhat<R, typeof neverReturn> extends true
+      ? never
+      : IsWhat<R, typeof anyReturn> extends true
+        ? any
+        : never
+  : never
 
 declare module '@typp/core' {
   namespace t {
@@ -14,44 +85,32 @@ declare module '@typp/core' {
     export interface ValidateTransformEntries<T, Input, InputRest> {
       number: [
         [T] extends [number] ? true : false,
-        Switch<{
-          any: [IsEqual<Input, any>, unknown]
-          number: [
-            IsAnySubType<Input, number | Number>,
-            [InputRest] extends [never] ? (
-              Input extends infer UnionInputItem ? (
-                IsEqual<UnionInputItem, Number> extends true
-                  ? number
-                  : Extract<UnionInputItem, number>
-              ) : never
+        SwitchBaseType<Input, InputRest, 'number', {
+          // TODO infer narrow number
+          // `${InputRest & bigint}` extends `${infer T extends number}` ? T : never,
+          bigint: number
+          boolean: InputRest extends true ? 1 : InputRest extends false ? 0 : never
+          number: [InputRest] extends [never] ? (
+            Input extends infer UnionInputItem ? (
+              IsEqual<UnionInputItem, Number> extends true
+                ? number
+                : Extract<UnionInputItem, number>
             ) : never
-          ]
-          bigint: [
-            IsAnySubType<InputRest, bigint | BigInt>,
-            number,
+          ) : never
+          string: InputRest extends (
             // TODO infer narrow number
-            // `${InputRest & bigint}` extends `${infer T extends number}` ? T : never,
-          ]
-          string: [
-            IsAnySubType<InputRest, string | String>,
-            InputRest extends (
-              // TODO infer narrow number
-              // | `${infer T extends number}${string}`
-              | `${number}${string}`
-              | `0${'b' | 'B'}${string}`
-              | `0${'o' | 'O'}${number}`
-              | `0${'x' | 'X'}${string}`
-            ) ? number
-              : true extends IsEqual<InputRest, string>
-                ? unknown
-                : never,
-          ]
-          boolean: [
-            IsAnySubType<InputRest, boolean | Boolean>,
-            InputRest extends true ? 1 : InputRest extends false ? 0 : never
-          ]
-          null: [IsSubType<InputRest, null>, 0]
-          undefined: [IsSubType<InputRest, undefined>, 0]
+            // | `${infer T extends number}${string}`
+            | `${number}${string}`
+            | `0${'b' | 'B'}${string}`
+            | `0${'o' | 'O'}${number}`
+            | `0${'x' | 'X'}${string}`
+          ) ? number
+            : true extends IsEqual<InputRest, string>
+              ? unknown
+              : never
+          symbol: never
+          null: 0
+          undefined: 0
         }>
       ]
     }
