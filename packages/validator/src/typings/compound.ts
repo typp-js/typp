21432fcbc5b0ef70cb9d905f3f1e8487699fc62f
YiJie <yijie4188@gmail.com>
2024-10-31 09:59:54 +0800

    feat(validator): implement compound validator

--- packages/validator/src/typings/compound.ts
+++ packages/validator/src/typings/compound.ts
@@ -1,2 +1,42 @@
+import { t, t as tn } from '@typp/core/base'
+import isWhatSpecialShapeSkm = t.isWhatSpecialShapeSkm
+
 // TODO union
 // TODO intersection
+
+// dprint-ignore
+declare module '@typp/core/base' {
+  namespace t {}
+}
+
+export function unionValidator(t: typeof tn) {
+  const { ValidateError } = t
+  t.useValidator((s): s is tn.Schema<
+    tn.SpecialShape<
+      tn.SpecialShapeTypeMapping['union'],
+      tn.Schema<unknown, unknown>[]
+    >,
+    any
+  > => isWhatSpecialShapeSkm('union', s), {
+    validate(input, options) {
+      const { schemas } = this.shape
+      let result: tn.ValidateErrorResult
+      for (const schema of schemas) {
+        const r = schema.tryValidate(input)
+        if (r.success) {
+          return true
+        } else {
+        }
+        result = r.error
+      }
+      throw new ValidateError(
+        'union',
+        this,
+        input,
+        'ValidateError:union not match any schemas',
+        [schemas]
+      )
+    },
+    transform: i => i
+  })
+}
