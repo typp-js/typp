81187b1d824c201391fef8b8a93bae1d873d2331
YiJie <yijie4188@gmail.com>
2024-11-12 13:42:01 +0800

    chore(docs): refactor to `type:module`

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -4,29 +4,31 @@ import loader from '@monaco-editor/loader'
 
 import DefaultTheme from 'vitepress/theme'
 
-loader.config({
-  paths: {
-    vs: 'https://cdn.jsdelivr.net/npm/monaco-editor@0.52.0/min/vs'
-  }
-})
-
-// observe html lang change and update availableLanguages
-new MutationObserver(async () => {
+if (!import.meta.env.SSR) {
   loader.config({
-    'vs/nls': {
-      availableLanguages: {
-        '*': {
-          'zh-Hans': 'zh-cn',
-          'zh-Hant': 'zh-tw',
-          en: 'en'
-        }[document.querySelector('html')?.lang || 'zh-Hans']
-      }
+    paths: {
+      vs: 'https://cdn.jsdelivr.net/npm/monaco-editor@0.52.0/min/vs'
     }
   })
-}).observe(document.querySelector('html')!, {
-  attributes: true,
-  attributeFilter: ['lang']
-})
+
+  // observe html lang change and update availableLanguages
+  new MutationObserver(async () => {
+    loader.config({
+      'vs/nls': {
+        availableLanguages: {
+          '*': {
+            'zh-Hans': 'zh-cn',
+            'zh-Hant': 'zh-tw',
+            en: 'en'
+          }[document.querySelector('html')?.lang || 'zh-Hans']
+        }
+      }
+    })
+  }).observe(document.querySelector('html')!, {
+    attributes: true,
+    attributeFilter: ['lang']
+  })
+}
 
 export default {
   extends: DefaultTheme
