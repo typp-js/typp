21b6f8fda104a19966f18f004d34f0698170843d
YiJie <yijie4188@gmail.com>
2024-11-13 04:47:59 +0800

    feat(docs): proxy client playground component

---
+++ docs/.vitepress/components/Playground.vue
@@ -0,0 +1,13 @@
+<template>
+  <PlaygroundClient v-bind="$attrs" />
+</template>
+
+<script lang="ts" setup>
+import { defineClientComponent } from 'vitepress'
+
+const slots = defineSlots()
+const PlaygroundClient = defineClientComponent(
+  () => import('#components/Playground.client.vue'),
+  [{}, slots]
+)
+</script>
